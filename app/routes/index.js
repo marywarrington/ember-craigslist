ce3f2fa2cf76367165b0d1ad3da053b778df0011
Marika Allely and Mary Warrington <marika@>
2016-04-04 11:37:55 -0700

    made category route and displayed categories on index

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      categories: this.store.findAll('category'),
+      posts: this.store.findAll('post')
+    });
+  },
+  actions: {
+    // save category function here
+  }
 });
