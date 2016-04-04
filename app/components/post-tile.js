82ec271b47b9443c32a5a38961e34c99b004a35f
Marika Allely and Mary Warrington <marika@>
2016-04-04 14:26:24 -0700

    add fullTitle computed component

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  fullTitle: Ember.computed('post.title', 'post.price', function() {
+    return this.get('post.title') + ": $" + this.get('post.price');
+  }),
   actions: {
     delete(post) {
       if(confirm('Are you sure?')) {
--- app/routes/category.js
+++ app/routes/category.js
@@ -6,11 +6,8 @@ export default Ember.Route.extend({
   },
   actions: {
     save(params) {
-      debugger;
       var newPost = this.store.createRecord('post', params);
       var category = params.category;
-
-      console.log(params);
       category.get('posts').addObject(newPost);
       newPost.save().then(function(){
         return category.save();
