202ccbf153a068b7a2194b1e3fda4ade8f143b59
Marika Allely and Mary Warrington <marika@>
2016-04-04 16:28:59 -0700

    end of day commit

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -5,6 +5,10 @@ export default Ember.Component.extend({
   fullTitle: Ember.computed('post.title', 'post.price', function() {
     return this.get('post.title') + ": $" + this.get('post.price');
   }),
+
+  sortBy: ['date:desc'],
+  sortedPosts: Ember.computed.sort('category.posts', 'sortBy'),
+  
   actions: {
     delete(post) {
       if(confirm('Are you sure?')) {
